9d044f9f484ea5086eee5fa7a336fcd4c2e49993
Obomo <yougay>
2021-08-17 17:28:17 -0700

    b

--- CreepDodge/HUD.gd
+++ CreepDodge/HUD.gd
@@ -12,8 +12,7 @@ func show_game_over():
 	yield($MessageTimer, "timeout") # wait for MessageTimer to count down, show game over for 2 seconds
 	$Message.text = "Dodge the \nCreeps!" # then return to title page, 1 second later play button becomes visible
 	$Message.show()
-	yield(get_tree().create_timer(1), "timeout") # create a one time timer and wait for it to finish
-	#instead of creating a timer node, you can use SceneTree's create_timer() for delays
+	$Menu.play()
 	$StartButton.show()
 
 func update_score(score):
@@ -21,10 +20,11 @@ func update_score(score):
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Menu.play()
 
 func _on_StartButton_pressed():
 	$StartButton.hide()
+	$Menu.stop()
 	emit_signal("start_game")
 
 func _on_MessageTimer_timeout():
